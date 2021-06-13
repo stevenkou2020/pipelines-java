6f8a4b5b481260a58b9c7362e481d9b5ba90db3e
stevenkou2020 <61336393+stevenkou2020@users.noreply.github.com>
2021-06-13 18:49:15 +0800

    Update Demo.java

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -3,10 +3,10 @@ package com.microsoft.demo;
 public class Demo {
     public void DoSomething(boolean flag){
         if(flag){
-            System.out.println("I am covered--hello world");
+            System.out.println("I am covered--hello world--koumr");
             return;
         }
 
-        System.out.println("I am not covered-hello world not");
+        System.out.println("I am not covered-hello world not--koumr");
     }
 }
